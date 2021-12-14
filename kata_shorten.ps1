54ef3f2e277b0d39b048f39ffcbdf2323918d110
Andreas Grimme <andreas.grimme@siemens-healthineers.com>
2021-12-14 15:40:16 +0100

    missing init

--- kata_shorten.ps1
+++ kata_shorten.ps1
@@ -1,2 +1,3 @@
-﻿gc $args[0].Split("`n")|%{$e=$_.Split("`t");$h[[char]$e[0].ToUpper()]=$e[1]}
+﻿$h=@{}
+gc $args[0].Split("`n")|%{$e=$_.Split("`t");$h[[char]$e[0].ToUpper()]=$e[1]}
 [char[]](Read-Host).ToUpper()|%{Write-Host -n $h[$_] ""}
