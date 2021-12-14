d975e3189c8967895fdc8711b6c0834a197064d5
Andreas Grimme <andreas.grimme@siemens-healthineers.com>
2021-12-14 14:50:13 +0100

    super-short powershell solution

---
+++ kata.ps1
@@ -0,0 +1,18 @@
+﻿$param1=$args[0]
+$hash = @{}
+
+$content = Get-Content -Path $param1
+$content.Split([Environment]::NewLine) | ForEach-Object {
+    $entry = $_.Split("`t")
+    $hash[$entry[0].ToUpper()] = $entry[1]   
+}
+
+$userInput = Read-Host
+$output = ""
+foreach($c in [char[]]$userInput.ToUpper()){
+    if($hash.Contains([string]$c)){
+        $output = $output + $hash[[string]$c] + " "
+    }
+}
+
+$output
---
+++ kata_shorten.ps1
@@ -0,0 +1,2 @@
+﻿gc $args[0].Split("`n")|%{$e=$_.Split("`t");$h[[char]$e[0].ToUpper()]=$e[1]}
+[char[]](Read-Host).ToUpper()|%{Write-Host -n $h[$_] ""}
