cdbb50404800b44ac590d842d29bff227d69d231
Andreas Grimme <andreas.grimme@siemens-healthineers.com>
2021-12-14 14:42:23 +0100

    C# solution

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿var spellFile = Environment.GetCommandLineArgs()[1];
+
+Dictionary<char, string> ReadSpellFile(string spellFile)
+{
+    return File.ReadAllText(spellFile).Split('\n')
+        .Select(_ => _.Trim())
+        .Select(_ => _.Split('\t'))
+        .ToDictionary(_ => Char.ToUpper(_[0][0]), _ => _[1]);
+}
+
+var spelling = ReadSpellFile(spellFile);
+
+while (true)
+{
+    var line = Console.ReadLine();
+    if (line is null) { break; }
+    foreach (var c in line)
+    {
+        if (spelling.TryGetValue(Char.ToUpper(c), out var text))
+        {
+            Console.Write(text); Console.Write(" ");
+        }
+    }
+    Console.WriteLine();
+}
